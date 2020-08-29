a10fce7b46a07cce61f388bb29eab42681e4b426
jneilliii <jneilliii+github@gmail.com>
2020-08-29 00:09:25 -0400

    switch to python format for command substitution

--- octoprint_mqttsubscribe/__init__.py
+++ octoprint_mqttsubscribe/__init__.py
@@ -100,26 +100,31 @@ class MQTTSubscribePlugin(octoprint.plugin.SettingsPlugin,
 			except Exception as e:
 				self._plugin_manager.send_plugin_message(self._identifier, dict(error=str(e)))
 
-	def _substitute (self, s, matches):
-		ls = []
-		isEscaped = False
-		for c in s:
-			if isEscaped:
-				if c == '\\':
-					ls.append ('\\')
-				elif c == '0':
-					ls.append (json.dumps (matches))
-				elif c.isdigit ():
-					ls.append (json.dumps (matches[int (c) - 1]))
-				else:
-					raise ValueError ('Command field contains invalid escape syntax: \\' + c)
-				isEscaped = False
-			else:
-				if c == '\\':
-					isEscaped = True
-				else:
-					ls.append (c)
-		return ''.join (ls)
+	def _substitute(self, s, matches):
+		# ls = []
+		# isEscaped = False
+		# for c in s:
+		# 	if isEscaped:
+		# 		if c == '\\':
+		# 			ls.append('\\')
+		# 		elif c == '0':
+		# 			ls.append(json.dumps(matches))
+		# 		elif c.isdigit():
+		# 			ls.append(json.dumps(matches[int (c) - 1]))
+		# 		else:
+		# 			raise ValueError('Command field contains invalid escape syntax: \\' + c)
+		# 		isEscaped = False
+		# 	else:
+		# 		if c == '\\':
+		# 			isEscaped = True
+		# 		else:
+		# 			ls.append(c)
+		# return ''.join(ls)
+		if s.startswith("{"):
+			command_string = "{" + t["command"] + "}"
+		else:
+			command_string = t["command"]
+		return command_string.format(*matches)
 
 	def _on_mqtt_subscription(self, topic, message, retained=None, qos=None, *args, **kwargs):
 		self._logger.debug("Received from %s|%s" % (topic, message))
@@ -130,7 +135,7 @@ class MQTTSubscribePlugin(octoprint.plugin.SettingsPlugin,
 				try:
 					address = "localhost"
 					port = self.port
-					headers = {'Content-type': 'application/json','X-Api-Key': self._settings.get(["api_key"])}
+					headers = {'Content-type': 'application/json', 'X-Api-Key': self._settings.get(["api_key"])}
 					# parse message extraction expression
 					extract = t["extract"]
 					expr = jsonpath_rw.parse (extract if extract else '$')
@@ -141,7 +146,7 @@ class MQTTSubscribePlugin(octoprint.plugin.SettingsPlugin,
 						args = []
 					# substitute matches in command
 					data = self._substitute (t["command"], args)
-					url = "http://%s:%s/%s" % (address,port,t["rest"])
+					url = "http://%s:%s/%s" % (address, port, t["rest"])
 					if t["type"] == "post":
 						r = requests.post(url, data=data, headers=headers)
 						self.mqtt_publish(t["topic"] + "/response", '{ "status" : %s, "response" : %s "data" : %s }' % (r.status_code, r.text, data))
